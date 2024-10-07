3cf9471b5ba71c93d522a7344b8595b4b2fcecb4
solboy <35817862+so111boy@users.noreply.github.com>
2024-10-07 19:47:24 +0100

    Update site name to Silk Road Dog

--- src/components/constants.ts
+++ src/components/constants.ts
@@ -1,15 +1,9 @@
 const plainurl = 'yuuire.com'
 
 export const meta = {
-    author: 'yuuire',
-    title: 'yuuire',
-    description: 'Fullstack Unicorn Developer',
-    plainurl: plainurl,
-    guide: 'https://guide.' + plainurl,
-    repo: 'https://github.com/yuuire/silk-road-marketplace',
-    email: 'yuuire//airmail.cc',
-    sponsor: 'https://liberapay.com/yuuire/donate',
-    matrix: '@yuuire:envs.net',
+    author: '$srDog',
+    title: 'Silk Road Dog',
+    description: '$srDog is the dog of Ross. Free Ross Ulbricht',
 }
 
 export const repositories = ["yuuire/guide", "yuuire/silk-road-marketplace"];
